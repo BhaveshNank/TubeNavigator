d2c973b1aa0614d7fb306da8be7d75a26fc8d5fa
Bhavesh Nankani <bhavesh@Bhaveshs-MacBook-Pro.local>
2023-11-07 22:48:02 +0000

    initial commit

---
+++ loader.py
@@ -0,0 +1,27 @@
+import csv
+
+def load_graph_from_csv(csv_filename):
+    graph = {}
+    with open(csv_filename, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if len(row) < 4 or not row[3].strip().isdigit():  # Check for empty string or non-digit duration
+                continue
+            line, start, end, duration = row
+            if start not in graph:
+                graph[start] = {}
+            if end not in graph:
+                graph[end] = {}
+            graph[start][end] = int(duration)
+            # Assuming the graph is undirected and travel time is the same in both directions
+            graph[end][start] = int(duration)
+    
+    return graph
+
+    
+
+graph = load_graph_from_csv("London_underground_data.csv")
+
+for start, destinations in graph.items():
+    for end, duration in destinations.items():
+        print("From {} to {}: {} minutes".format(start, end, duration))
