893634476710cc23e7bc7936241fc27408ebbd2b
Bhavesh Nankani <bhavesh@Bhaveshs-MacBook-Pro.local>
2023-11-09 15:06:33 +0000

    tasks 1 and 2 all completed

--- task2.py
+++ task2.py
@@ -4,26 +4,30 @@ def dijkstra(graph, start):
     distances = {vertex: float('infinity') for vertex in graph}
     distances[start] = 0
 
+    # The priority queue stores entries as (distance, vertex)
     priority_queue = [(0, start)]
     predecessors = {vertex: None for vertex in graph}
-    
+
+    # Set to keep track of visited nodes to prevent processing a node more than once
+    visited = set()
 
     while priority_queue:
         current_distance, current_vertex = heapq.heappop(priority_queue)
-        
-        if current_distance > distances[current_vertex]:
+        if current_vertex in visited:
             continue
+        visited.add(current_vertex)
 
-        
+        # Iterate over the neighbors of the current node
         for neighbor in graph[current_vertex]:
             distance = current_distance + 1  # Since each edge represents 1 stop
-            
+
+            # Only consider this new path if it's better than any path we've
+            # already found
             if distance < distances[neighbor]:
                 distances[neighbor] = distance
                 predecessors[neighbor] = current_vertex
                 heapq.heappush(priority_queue, (distance, neighbor))
-                print(f"Updated distance to {neighbor}: {distance}")
-                
+
     return distances, predecessors
 
 
