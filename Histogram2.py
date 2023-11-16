97853a339bb035759bd85aae166b76b261889622
Bhavesh Nankani <bhavesh@Bhaveshs-MacBook-Pro.local>
2023-11-16 22:40:49 +0000

    tasks1 and 2 corrected

--- Histogram2.py
+++ Histogram2.py
@@ -1,15 +1,19 @@
 import matplotlib.pyplot as plt
 from itertools import combinations
 from loader2 import load_graph_from_csv
-from task2 import dijkstra
+from dijkstra import dijkstra
+from main2 import convert_graph
+import time 
 
+start = time.time()
 def calculate_all_pairs_stops(graph):
     all_stops = []
-    for start_station, destination_station in combinations(graph.keys(), 2):
-        distances, _ = dijkstra(graph, start_station)
-        if destination_station in distances:
-            all_stops.append(distances[destination_station])
+    for start_index, destination_index in combinations(range(graph.get_card_V()), 2):
+        distances, _ = dijkstra(graph, start_index)
+        if distances[destination_index] < float('infinity'):
+            all_stops.append(distances[destination_index])
     return all_stops
+
 
 def plot_histogram(stops_counts):
     plt.hist(stops_counts, bins=range(1, max(stops_counts) + 2), edgecolor='black')
@@ -19,9 +23,13 @@ def plot_histogram(stops_counts):
     plt.show()
 
 def main():
-    graph = load_graph_from_csv("London_underground_data.csv")
+    dictionary_graph = load_graph_from_csv("London_underground_data.csv")
+    graph, _, _ = convert_graph(dictionary_graph)
     stops_counts = calculate_all_pairs_stops(graph)
     plot_histogram(stops_counts)
 
 if __name__ == "__main__":
     main()
+end = time.time()
+total_time = end-start
+print(total_time)
--- main2.py
+++ main2.py
@@ -1,29 +1,59 @@
 from loader2 import load_graph_from_csv
-from task2 import dijkstra
-
+from dijkstra import dijkstra
+# from print_path import print_path as print_path_func
+from adjacency_list_graph import AdjacencyListGraph
 
 def get_user_input():
     start_station = input("Please enter your starting station: ")
     destination_station = input("Please enter your destination station: ")
     return start_station, destination_station
 
+def convert_graph(dictionary_graph):
+    stations = list(dictionary_graph.keys())
+    station_to_index = {station: index for index, station in enumerate(stations)}
+    index_to_station = {index: station for station, index in station_to_index.items()}
+
+    graph = AdjacencyListGraph(len(stations), weighted=True)
+
+    for start_station, destinations in dictionary_graph.items():
+        u = station_to_index[start_station]
+        for end_station in destinations.keys():
+            v = station_to_index[end_station]
+            graph.insert_edge(u, v, 1)  # Each edge has a weight of 1
+
+    return graph, station_to_index, index_to_station
+
 def main():
-    graph = load_graph_from_csv("London_underground_data.csv")
-    # print(graph)
+    dictionary_graph = load_graph_from_csv("London_underground_data.csv")
+    graph, station_to_index, index_to_station = convert_graph(dictionary_graph)
+
     start_station, destination_station = get_user_input()
-    
-    if start_station not in graph or destination_station not in graph:
+
+    start_index = station_to_index.get(start_station)
+    destination_index = station_to_index.get(destination_station)
+
+    if start_index is None or destination_index is None:
         print("One or more of the stations entered do not exist in the network.")
         return
 
-    distances, _ = dijkstra(graph, start_station)
+    distances, predecessors = dijkstra(graph, start_index)
 
-    if destination_station in distances:
-        print("The number of stops from {} to {} is: {}".format(start_station, destination_station, distances[destination_station]))
+    # Check if a path exists to the destination
+    if distances[destination_index] == float('infinity'):
+        print(f"No path found from {start_station} to {destination_station}.")
     else:
-        print("Destination station is not reachable from the starting station.")
+        # Reconstruct the path from start_station to destination_station
+        path = []
+        current = destination_index
+        while current != None:
+            path.insert(0, index_to_station[current])
+            current = predecessors[current]
 
+        # Display the path and total number of stations
+        print("Path from {} to {}:".format(start_station, destination_station))
+        for station in path:
+            print(station)
+        print("Total number of stations: {}".format(distances[destination_index]))
 
 if __name__ == "__main__":
     main()
-    
